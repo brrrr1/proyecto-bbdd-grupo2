be5e8a83275c4914c98dfcb383c6be6acf08e79a
Zaid <pilarads06@gmail.com>
2024-02-22 21:58:09 +0100

    22 feb insert into

---
+++ FINAL/SQL/categoria.sql
@@ -0,0 +1,7 @@
+INSERT INTO "categoria" ("idCategoria", "nombre") VALUES
+	('1', 'frenos'),
+	('2', 'motor y embrague'),
+	('3', 'filtros'),
+	('4', 'bateria'),
+	('5', 'neumatico'),
+	('6', 'carroceria');
